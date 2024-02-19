c861553f1413b0cb361c90d9011ba594dbecdcd3
gabrieleli12345 <gabrielelimanacap@gmail.com>
2024-02-19 16:06:54 +0800

    changed the title back to STEMTask

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
         style={{ fontSize: "70px", fontWeight: "bold" }}
         className="text-primary"
       >
-        nigga{" "}
+        STEMTask{" "}
       </h1>
       <h3>A Task Management Web App</h3>
       <hr /> 
