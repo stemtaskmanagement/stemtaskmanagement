2eaf8f6ce4f1d84ddef9de718f4555cd79a696fb
unknown <gabrielelimanacap@gmail.com>
2024-03-18 15:38:41 +0800

    added survey and fixed calendar design

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -21,6 +21,8 @@ export default function Header({ userCredentials }) {
       <h3 style={{ paddingBottom: "25px" }}>
         A Task Management Web App for Grade 11 STEM Students
       </h3>
+      {console.log(userCredentials.length)}
+      {userCredentials.length == undefined ? <a href="https://l.messenger.com/l.php?u=https%3A%2F%2Fdocs.google.com%2Fforms%2Fd%2F1EmcD5O_BrzpV6i8Bnb_03sT2e6PqERBVA7FzpMccYMY%2Fedit&h=AT2PsNr_kWq7tiCl4KV5Ee7YEvtK-i9M12xSwcyZv98Rgor32DEha5BrKtSV82pEZ9B-B7zNF4A4AUKfv8IVt4RmLuT_OvYFa4t4lLEwXNGS4p-k1POip9wG9uBIyo4zb_F7Mw" target="_blank">Please Answer this Survey for our Research</a> : ""}
       {userCredentials.length == undefined ? (
         <Button
           name="Start Creating Tasks"
@@ -28,7 +30,7 @@ export default function Header({ userCredentials }) {
           color="btn-primary"
         />
       ) : (
-        <Button name="Get Started" href="/login" color="btn-primary" />
+        <div><Button name="Get Started" href="/login" color="btn-primary" /></div>
       )}
     </div>
   );
